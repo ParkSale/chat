70bc12cc8576a9f3bf630f736c198fb0ca981ee0
ParkSale <34956785+ParkSale@users.noreply.github.com>
2020-03-23 22:48:48 +0900

    Initial commit

---
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -0,0 +1,14 @@
+package com.example.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ChatController {
+
+    @GetMapping("/")
+    public String chat(){
+        return "chat";
+    }
+}
